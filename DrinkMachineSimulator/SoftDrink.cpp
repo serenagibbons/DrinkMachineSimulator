97ba7ccdc292c3cc29c816cd4e9ee820419aa626
Serena Gibbons <serenalgibbons@gmail.com>
2019-06-08 15:18:20 -0400

    Add for loop to displayChoices function

--- DrinkMachineSimulator/SoftDrink.cpp
+++ DrinkMachineSimulator/SoftDrink.cpp
@@ -7,7 +7,6 @@ drink::drink(string n, double c, int num) {		// Drink constructor
 	numAvailable = num;
 }
 
-
 drink::drink() {								// Drink default constructor
 	name = " ";
 	cost = 0.00;
@@ -82,11 +81,10 @@ void SoftDrink::displayChoices() {	// Display a menu of drink names and prices
 	cout << star << endl;
 
 	cout << fixed << showpoint << setprecision(2);
-	cout << "1. " << setw(18) << array[0].name << "$" << array[0].cost << endl;
-	cout << "2. " << setw(18) << array[1].name << "$" << array[1].cost << endl;
-	cout << "3. " << setw(18) << array[2].name << "$" << array[2].cost << endl;
-	cout << "4. " << setw(18) << array[3].name << "$" << array[3].cost << endl;
-	cout << "5. " << setw(18) << array[4].name << "$" << array[4].cost << endl << endl;
+	for (int i = 0; i < 5; i++)
+		cout << i + 1 << ". " << setw(18) << array[i].name << "$" << array[i].cost << endl;
+
+	cout << endl;
 }
 
 void SoftDrink::buyDrink(int sel) {	// Handles a sale
@@ -108,6 +106,5 @@ void SoftDrink::buyDrink(int sel) {	// Handles a sale
 		array[sel].numAvailable--;
 		cout << "Here is your beverage.\n\n";
 	}
-	dailyReport();
 
 }
